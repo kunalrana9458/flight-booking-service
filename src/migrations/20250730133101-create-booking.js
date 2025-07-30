7deb76693b833b9c01781ef687877dc566fc3ed6
kunalrana9458 <kunalranabsr9458@gmail.com>
2025-07-30 22:15:04 +0530

    creation of  booking model in the flight booking service

---
+++ src/migrations/20250730133101-create-booking.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+const {Enums} = require('../utils/common')
+const {BOOKED,CANCELLED,PENDING,INITIATED} = Enums.BOOKING_STATUS
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Bookings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      flightId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.ENUM,
+        allowNull: false,
+        values: [INITIATED,PENDING,BOOKED,CANCELLED],
+        defaultValue: INITIATED
+      },
+      noOfSeats: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 1
+      },
+      totalCost: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Bookings');
+  }
+};
