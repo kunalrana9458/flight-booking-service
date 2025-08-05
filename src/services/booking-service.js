31c893d5ace43ccef66e0c6b8abbc2f7b0e07b2c
kunalrana9458 <kunalranabsr9458@gmail.com>
2025-08-05 10:05:01 +0530

    Added Cancel booking logic with increase noOfSeats after cancellation

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -46,7 +46,7 @@ async function makePayment(data){
         const bookingTime = new Date(bookingDetails.createdAt)
         const currentTime = new Date()
         if(currentTime-bookingTime > 30000){
-            await bookingRepository.update(data.bookingId,{status:CANCELLED},transaction)
+            await cancelBooking(data.bookingId)
             throw new AppError('The Booking has Expired',StatusCodes.BAD_REQUEST)
         }
         if(bookingDetails.totalCost !== data.totalCost){
@@ -64,8 +64,29 @@ async function makePayment(data){
         throw error
     }
 }
- 
+
+async function cancelBooking(bookingId){
+    const transaction = await db.sequelize.transaction()
+    try {
+        const bookingDetails = await bookingRepository.get(bookingId,transaction)
+        if(bookingDetails.status == CANCELLED){
+            await transaction.commit()
+            return true
+        }
+        await axios.patch(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${bookingDetails.flightId}/seats`,{
+            seats: bookingDetails.noOfSeats,
+            dec:0
+        })
+        await bookingRepository.update(bookingId,{status:CANCELLED},transaction)
+        await transaction.commit()
+    } catch (error) {
+        await transaction.rollback()
+        throw error
+    }
+}
+
 module.exports = {
   createBooking,
-  makePayment
+  makePayment,
+  cancelBooking
 };
