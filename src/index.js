f561bbb1f55a481d94f083682abf7a05899be1d5
kunalrana9458 <kunalranabsr9458@gmail.com>
2025-08-10 22:22:44 +0530

    setup od redis server for including the caching functionality

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 const express = require('express')
 
-const {ServerConfig} = require('./config')
+const {ServerConfig,RedisServer} = require('./config')
 const apiRoutes = require('./routes')
 const scheduleCrons = require('./utils/common/cron-jobs')
+
+const {connectRedis} = RedisServer
+
 const app = express()
+
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/api',apiRoutes)
 
-app.listen(ServerConfig.PORT,() => {
-    console.log(`Successfully Started the server on PORT: ${ServerConfig.PORT}`); 
-    scheduleCrons()
-});
+const startServer = async () => {
+    try {
+        await connectRedis()
 
+        app.listen(ServerConfig.PORT, () => {
+            console.log(`Server started on PORT : ${ServerConfig.PORT}`)
+            scheduleCrons()
+        })
+    } catch (error) {
+        console.log('Failed to start the server:',error)
+        process.exit(1)
+    }
+}
+
+startServer()
